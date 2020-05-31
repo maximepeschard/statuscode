d6247baa9cbbb51344fd9d173cac46d10488f5e6
Maxime Peschard <maxime.peschard@gmail.com>
2020-05-31 23:26:45 +0200

    Handle graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -36,9 +39,22 @@ func main() {
 		IdleTimeout:  time.Second * 60,
 	}
 
+	// Server runs in a goroutine so that it does not block.
+	go func() {
 	log.Info().Str("addr", server.Addr).Msg("starting server")
-	err = server.ListenAndServe()
-	if err != nil {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Send()
+		}
+	}()
+
+	// We handle graceful shutdowns for SIGINT and SIGTERM.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	log.Info().Msg("shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal().Err(err).Send()
 	}
 }
